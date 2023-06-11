50dcf4c5bdd50d45cf35f91fc6790e6a147d75cd
Calum Smeaton <smeatsbeats@gmail.com>
2023-06-11 19:25:38 +0100

    slow but sensible RFID recognition to change addressable RGB LED profile

--- ELEGOO_TUTORIALS/ELEGOO_TUTORIALS/src/main.cpp
+++ ELEGOO_TUTORIALS/ELEGOO_TUTORIALS/src/main.cpp
@@ -1,27 +1,190 @@
-//www.elegoo.com
-//2016.12.08
+/*
+ * 
+ * All the resources for this project: https://randomnerdtutorials.com/
+ * Modified by Rui Santos
+ * 
+ * Created by FILIPEFLOP
+ * 
+ */
+ 
+#include <SPI.h>
+#include <MFRC522.h>
+#include <map>
+#include <Adafruit_NeoPixel.h>
 
-#include "Arduino.h"
-#include "pitches.h"
- 
-// notes in the melody:
-int melody[] = {
-  NOTE_C5, NOTE_D5, NOTE_E5, NOTE_F5, NOTE_G5, NOTE_A5, NOTE_B5, NOTE_C6};
-int duration = 500;  // 500 miliseconds
- 
-void setup() {
- 
+
+#define SS_PIN 5
+#define RST_PIN 21
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
+
+#define PIN 25
+#define NUMPIXELS 10
+Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void colorWipe(uint32_t c, uint8_t wait);
+
+int current_profile;
+
+void setup() 
+{
+  // LED test
+  pixels.begin();
+  pixels.setBrightness(50);
+  pixels.show(); // Initialize all pixels to 'off'
+
+  Serial.begin(9600);   // Initiate a serial communication
+  SPI.begin();      // Initiate  SPI bus
+  mfrc522.PCD_Init();   // Initiate MFRC522
+  Serial.println("Approximate your card to the reader...");
+  Serial.println();
 }
- 
-void loop() {  
-  for (int thisNote = 0; thisNote < 8; thisNote++) {
-    // pin8 output the voice, every scale is 0.5 sencond
-    tone(26, melody[thisNote], duration);
-     
-    // Output the voice after several minutes
-    delay(1000);
+void loop() 
+{
+
+  // LED test 
+
+  //LED test
+  /*
+  byte r = random(0,60);
+  byte g = random(0,60);
+  byte b = random(0,60);
+  for(int i=0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(r, g, b));
+    pixels.show();   // Send the updated pixel colors to the hardware.
+    delay(10); // Pause before next pass through loop
   }
-   
-  // restart after two seconds 
-  delay(2000);
+  */
+
+
+  // Look for new cards
+  if ( ! mfrc522.PICC_IsNewCardPresent()) 
+  {
+    //Serial.println("No new card?");
+    //This means no card is presented to the reader - can be same card twice
+    // You want the animations to go in here so they don't get interrupted by each loop 
+
+    switch (current_profile) {
+    case 1:
+
+      //Serial.println("Loading BlueFob Profile.");
+
+      //colorWipe(pixels.Color(0, 0, 255), 50); //blue
+    
+        for(int i=0; i<NUMPIXELS; i++) {
+          if ( ! mfrc522.PICC_IsNewCardPresent()) { // if a card is presented during the animation - stop it
+            pixels.setPixelColor(i, pixels.Color(0, 0, 60));
+            pixels.show();   // Send the updated pixel colors to the hardware.
+            delay(10); // Pause before next pass through loop
+          }
+        }
+      break;
+    case 2:
+
+      //Serial.println("Loading WhiteCard Profile.");
+
+      //colorWipe(pixels.Color(255, 255, 255), 50); //white
+
+      for(int i=0; i<NUMPIXELS; i++) {
+        if ( ! mfrc522.PICC_IsNewCardPresent()) {
+          pixels.setPixelColor(i, pixels.Color(60, 60, 60));
+          pixels.show();   // Send the updated pixel colors to the hardware.
+          delay(10); // Pause before next pass through loop
+        }
+      }
+      break;
+    default:
+      Serial.println("No profile found.");
+      //colorWipe(pixels.Color(255, 0, 0), 50); //red
+      byte r = random(0,60);
+      byte g = random(0,60);
+      byte b = random(0,60);
+      for(int i=0; i<NUMPIXELS; i++) {
+        if ( ! mfrc522.PICC_IsNewCardPresent()) {
+          pixels.setPixelColor(i, pixels.Color(r, g, b));
+          pixels.show();   // Send the updated pixel colors to the hardware.
+          delay(10); // Pause before next pass through loop
+        }
+      }
+  }
+
+    return;
+  }
+  else {
+    Serial.println("New card detected!");
+  }
+  // Select one of the cards
+  if ( ! mfrc522.PICC_ReadCardSerial()) 
+  {
+    return;
+  }
+  //Show UID on serial monitor
+  Serial.print("UID tag :");
+  String content= "";
+  byte letter;
+  for (byte i = 0; i < mfrc522.uid.size; i++) 
+  {
+     Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
+     Serial.print(mfrc522.uid.uidByte[i], HEX);
+     content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
+     content.concat(String(mfrc522.uid.uidByte[i], HEX));
+  }
+
+  content.toUpperCase();
+  String PID = content.substring(1);
+  Serial.println("The PID is: " + PID);
+
+  // create map of IDs and Profile names 
+  std::map<String, String> profiles{{"94 D5 E9 85", "BlueFob"}, {"9A AF 7D D4", "WhiteCard"}};
+
+  // find function will return iterable containing key value pair
+  // if the desired value is not found, it returns an iterable that represents a point beyond the end of the map
+  auto iter = profiles.find(PID);
+
+  if(iter != profiles.end()){
+    Serial.println("Load profile: " + iter->second);
+    delay(2000);
+
+    
+    if(iter->second == "BlueFob") {
+
+      current_profile = 1;
+
+    }
+    else if(iter->second == "WhiteCard") {
+
+      current_profile = 2;
+
+    }
+  
+  }
+  else {
+    Serial.println("No profile found.");
+    delay(2000);
+  }
+  
+
+  /*
+  if (content.substring(1) == "BD 31 15 2B") //change here the UID of the card/cards that you want to give access
+  {
+    Serial.println("Authorized access");
+    Serial.println();
+    delay(3000);
+  }
+ 
+ else   {
+    Serial.println(" Access denied");
+    delay(3000);
+  }
+  */
+
+} 
+
+// Fill the dots one after the other with a color
+
+void colorWipe(uint32_t c, uint8_t wait) {
+  for(uint16_t i=0; i<pixels.numPixels(); i++) {
+    pixels.setPixelColor(i, c);
+    pixels.show();
+    delay(wait);
+  }
 }
